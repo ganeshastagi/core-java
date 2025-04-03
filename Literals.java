97fc797a5377286dbebc227db1c5d1715ebd1f69
ganeshastagi <Ganeshastagi6@gmail.com>
2025-04-03 11:40:44 -0400

    Added Literals

---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Literals {
+    public static void main(String[] args) {
+
+        // Literals in Java are fixed values that are assigned to variables. They represent constant values directly in code.
+
+        int x = 5;
+        float y = 3.14f;
+        char c = 'A';
+        String s = "Hello";
+        boolean b = true;
+
+        // They are literally the values.
+    }
+}
