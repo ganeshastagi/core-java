a1fbb4227edf679bdbc240bc171db6c068d14f44
ganeshastagi <Ganeshastagi6@gmail.com>
2025-04-03 11:34:50 -0400

    Modified First program

--- First.java
+++ First.java
@@ -1,8 +1,5 @@
 class First {
     public static void main (String args[]) {
-        System.out.println("Ganesh Astagi");
-        System.out.println("Harsoor");
-        System.out.println("Gulbarga");
-        System.out.println("Karnataka");
+        System.out.println("Hello World");
     }
 }
