f8b27f4ed7a82d16b3cb22d3855213dfddf5b089
ganeshastagi <Ganeshastagi6@gmail.com>
2025-04-03 11:33:15 -0400

    Variables and Datatypes

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Integers
+        byte a = -128;          // 1 byte - 8 bits - Range = -128 to 127
+        short b = 32767;        // 2 bytes - 16 bits - Range = -32768 to 32767
+        int c = 40000;          // 4 bytes - 32 bits - Range = -2147483648 to 2147833647
+        long d = 123456789L;    // 8 bytes - 64 bits - Range = 19 digits
+
+        //Decimal
+        float f = 3.14f;        // 4 bytes - 32 bits
+        double g = 3.14159265358979323846;      // 8 bytes - 64 bits
+
+        //Character
+        char h = 'a';           // 2 bytes - 16 bits
+
+        //Boolean
+        boolean i = true;       // True or False
+    }
+}
---
+++ variables.java
@@ -0,0 +1,7 @@
+public class variables {
+    public static void main(String[] args) {
+        int a;              // Declare a variable
+        a = 200;            // Initialize a variable
+        System.out.println(a);
+    }
+}
