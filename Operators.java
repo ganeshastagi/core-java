425598c84f210553076822e9f53d28868841acd2
ganeshastagi <Ganeshastagi6@gmail.com>
2025-05-15 12:00:40 -0400

    Updated Operators program

--- Operators.java
+++ Operators.java
@@ -9,19 +9,19 @@ public class Operators {
         System.out.println(result);
 
         // Pre-Increment and Post-Increment - They behave different when you try to fetch the value.
-        //int pre_incre = ++a;
-        int post_incre = a++;
-        //System.out.println(pre_incre);
+        int pre_incre = ++a;        //a is fetched and incremented first & then assigned to the variable.
+        int post_incre = a++;       //a is fetched and assigned to variable, then the variable is incremented.
+        System.out.println(pre_incre);
         System.out.println(post_incre);
-        for(int i=0; i<5; i++) {
-            System.out.print(i);
-        }
-        System.out.println();
-        for(int i=0; i<5; ++i)
-        {
-            System.out.print(i);
-        }
-        System.out.println();
+//        for(int i=0; i<5; i++) {
+//            System.out.print(i);
+//        }
+//        System.out.println();
+//        for(int i=0; i<5; ++i)
+//        {
+//            System.out.print(i);
+//        }
+//        System.out.println();
 
         //Relational Operators - ==, !=, <, >, <=, >=
         int x = 10;
@@ -30,6 +30,14 @@ public class Operators {
         System.out.println(z);
 
         //Bitwise Operators - &, |, &&, ||, !
+        if(x > 5 || y > 7)
+        {
+            System.out.println("Ohoo! I started");
+        }
 
+        if(x < 5 && y > 7)
+        {
+            System.out.println("Thank you!");
+        }
     }
 }
