bfb8b76de40d51d9fb8a4f5dd8b12c2105bda983
ganeshastagi <Ganeshastagi6@gmail.com>
2025-05-15 17:02:18 -0400

    Nested Loops

---
+++ NestedLoops.java
@@ -0,0 +1,16 @@
+public class NestedLoops {
+    public static void main (String[] args) {
+        // Loop in a loop is called nested loops.
+
+        int i = 1;
+        while(i<=5) {
+            System.out.println("Hey, Gorgeous!");
+            int j=1;
+            while(j<=3){
+                System.out.println("How you doin!!");
+                j++;
+            }
+            i++;
+        }
+    }
+}
