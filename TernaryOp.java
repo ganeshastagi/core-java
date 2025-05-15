a0adb085310ca0cd7ccdc42ade9b32723c7fa4e2
ganeshastagi <Ganeshastagi6@gmail.com>
2025-05-15 14:04:03 -0400

    Ternary Operator program

---
+++ TernaryOp.java
@@ -0,0 +1,8 @@
+public class TernaryOp {
+    public static void main (String[] args) {
+        int a = 11;
+        String result;
+        result = a%2==0 ? "Even" : "Odd";
+        System.out.println(result);
+    }
+}
