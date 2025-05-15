23e1bb4534a3aeea104a8b7ae33e12049f89e0cb
ganeshastagi <Ganeshastagi6@gmail.com>
2025-05-15 13:59:15 -0400

    Added Conditional statement program

---
+++ CondStatements.java
@@ -0,0 +1,15 @@
+public class CondStatements {
+    public static void main (String[] args) {
+        //Conditional statements include if, if else, if, else if, else
+        // These checks for true or false
+        int a = 50;
+        int b = 60;
+        int c = 90;
+        if (a>b && a>c)
+            System.out.println(a);
+        else if (b>c)
+            System.out.println(b);
+        else
+            System.out.println(c);
+    }
+}
