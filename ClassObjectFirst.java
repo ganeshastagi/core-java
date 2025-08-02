200ab217e4f9a03fafed0d20da6aa2aa261158da
ganeshastagi <Ganeshastagi6@gmail.com>
2025-08-02 13:44:14 -0400

    Updated Class and Object first program

--- ClassObjectFirst.java
+++ ClassObjectFirst.java
@@ -2,13 +2,18 @@ public class ClassObjectFirst {
     public static void main (String[] a) {
         //Creating an object for the class car
         Car myCar = new Car();
+        Car myCar2 = new Car();
 
         //Setting fields
         myCar.brand = "Honda";
         myCar.speed = 100;
 
+        myCar2.brand = "Ford";
+        myCar2.speed = 120;
+
         //Calling method
         myCar.drive();
+        myCar2.drive();
 
     }
 }
