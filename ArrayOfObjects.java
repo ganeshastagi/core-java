de5f223975d5c3e5a0fe2f8f53fed314183b368d
ganeshastagi <Ganeshastagi6@gmail.com>
2025-08-05 17:08:54 -0400

    Added Array of objects and Enhanced For loop programs

---
+++ ArrayOfObjects.java
@@ -0,0 +1,40 @@
+class Student {
+    String name;
+    int age;
+}
+
+public class ArrayOfObjects {
+    public static void main(String[] args) {
+        // In Java, an array of objects means a regular array that stores references to instances of a class.
+        // This is useful when you want to manage multiple entities of the same type.
+
+        Student s1 = new Student();
+        s1.name = "Jim";
+        s1.age = 30;
+
+        Student s2 = new Student();
+        s2.name = "Dwight";
+        s2.age = 30;
+
+        Student s3 = new Student();
+        s3.name = "Andy";
+        s3.age = 32;
+
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for(int i=0; i<students.length; i++) {
+            System.out.println("Name: "+ students[i].name + " Age: " + students[i].age);
+        }
+
+        // Enhanced For loop
+        for(Student s : students) {
+            System.out.println("Name: " + s.name + " Age: " + s.age);
+        }
+
+    }
+}
+
---
+++ EnhancedForLoop.java
@@ -0,0 +1,18 @@
+public class EnhancedForLoop {
+    public static void main (String [] args) {
+        // Enhanced for loop or For each loop is used when we want to work directly with the data or the values in the array
+        // not with its index.
+        // If you want to work with index, you can use normal for loop.
+        // You can't modify the data, you can just access the data and display it.
+
+        int a[] = new int[4];
+        a[0] = 4;
+        a[1] = 3;
+        a[2] = 2;
+        a[3] = 1;
+
+        for(int n : a) {                    // int n is just a variable given to store the value
+            System.out.println(n);
+        }
+    }
+}
